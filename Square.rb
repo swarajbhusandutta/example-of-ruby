935f665d40f47fdc2acad214160c19d4c9c7b21e
github-developer <trainingdemos+github-developer@github.com>
2015-10-01 02:04:26 -0700

    documentation on square class

--- Square.rb
+++ Square.rb
@@ -3,10 +3,12 @@ class Square
   attr_reader :side
   attr_accessor :border
   
+  # Needed to create a new Square object
   def initialize(side)
     @side = side
   end
 
+  # Used to compute area quickly
   def area
     side*side
   end
